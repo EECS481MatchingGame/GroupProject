8e4c5b45429e84ffe0144bd2db8eab98989715c0
ZucchiniSky <scottbommarito@gmail.com>
2015-10-22 21:09:45 -0400

    card boundaries and locations
    18 cards on screen, 50 * 72 each, board is 600 by 440

--- KinectColorApp/CardController.cs
+++ KinectColorApp/CardController.cs
@@ -18,6 +18,10 @@ namespace KinectColorApp
     {
         // variables contained in card - we can add more!  Such as an image file, bool showOnBoard, etc
         public bool isPressed;
+
+        public static int width = 50;
+        public static int height = 72;
+
         public int topYcoordinate;
         public int bottomYcoordinate;
         public int leftXcoordinate;
@@ -26,13 +30,13 @@ namespace KinectColorApp
         public int centerYcoordinate;
 
         // constructor that takes all arguments
-        public CardController(bool isPrssd, int topY, int botY, int leftX, int rightX)
+        public CardController(bool isPrssd, int topY, int leftX)
         {
             isPressed = isPrssd;
             topYcoordinate = topY;
-            bottomYcoordinate = botY;
             leftXcoordinate = leftX;
-            rightXcoordinate = rightX;
+            bottomYcoordinate = topY + height;
+            rightXcoordinate = leftX + width;
             centerXcoordinate = (leftXcoordinate + rightXcoordinate) / 2;
             centerYcoordinate = (topYcoordinate + bottomYcoordinate) / 2;
         }
@@ -44,40 +48,28 @@ namespace KinectColorApp
         public void setBotY(int newBotY) { bottomYcoordinate = newBotY; }
         public void setLeftX(int newLeftX) { leftXcoordinate = newLeftX; }
         public void setRightX(int newRightX) { rightXcoordinate = newRightX; }
-
-        // later on, make this pass by reference
-        public static initializeCards (ref List<CardController> cards)
+        
+        public static void initializeCards (ref List<CardController> cards)
         {
             /* --------------- THE GAME BOARD IS ORGANIZED ASSUMING THE FOLLOWING CARD PLACEMENT -------------------
 
-                0       1       2       3       4
-                5       6       7       8       9
-                10      11      12      13      14
-                15      16      17      18      19
+                0       1       2       3       4       5
+                6       7       8       9       10      11
+                12      13      14      15      16      17
 
                 note that not all 20 cards need be displayed at once, however their locations and sizes are absolute
+                
+            board is 640w*480h, use margins of 20w and 20h
             ------------------------------------------------------------------------------------------------------*/
             // Hardcode in 20 cards, directly into the list that's returned
-            cards[0]  = new CardController(false, 0, 1, 2, 3);  // card 0 is top left
-            cards[1]  = new CardController(false, 0, 1, 2, 3);  // card 1 is second from left, top row
-            cards[2]  = new CardController(false, 0, 1, 2, 3);
-            cards[3]  = new CardController(false, 0, 1, 2, 3); 
-            cards[4]  = new CardController(false, 0, 1, 2, 3);  // card 4 is the top right
-            cards[5]  = new CardController(false, 0, 1, 2, 3);  // card 5 is the leftmost card in the second-from-the-top row
-            cards[6]  = new CardController(false, 0, 1, 2, 3);
-            cards[7]  = new CardController(false, 0, 1, 2, 3);
-            cards[8]  = new CardController(false, 0, 1, 2, 3);
-            cards[9]  = new CardController(false, 0, 1, 2, 3);
-            cards[10] = new CardController(false, 0, 1, 2, 3);
-            cards[11] = new CardController(false, 0, 1, 2, 3);
-            cards[12] = new CardController(false, 0, 1, 2, 3);
-            cards[13] = new CardController(false, 0, 1, 2, 3);
-            cards[14] = new CardController(false, 0, 1, 2, 3);
-            cards[15] = new CardController(false, 0, 1, 2, 3);
-            cards[16] = new CardController(false, 0, 1, 2, 3);
-            cards[17] = new CardController(false, 0, 1, 2, 3);
-            cards[18] = new CardController(false, 0, 1, 2, 3);
-            cards[19] = new CardController(false, 0, 1, 2, 3);  // card 19 is the bottom right card
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    cards[i * j + j] = new CardController(false, 20 + i * 2 * height, 20 + j * 2 * width);
+                }
+            }
         }
         
 
@@ -87,7 +79,7 @@ namespace KinectColorApp
         // will have to later update so that it's only looking at cards that are still on the board
         // if the location of the touch is within a card's boundaries, isPressed flips to true.
         // Not sure where to reset isPressed to false yet though
-        public static updatePressed (ref List <CardController> cards, short[] rawDepthData)
+        public static void updatePressed (ref List <CardController> cards, short[] rawDepthData)
         {
             // change the argument when i figure out what constitutes a coordinate - probably more specific than raw depth data
 
